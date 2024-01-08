864e4e7bd290501530c2b65a930a1a70721a2f90
gabriel <gabrielcomputacao@outlook.com>
2024-01-08 07:18:29 -0300

    feat:  creating safe area view

--- ignitefleet/App.tsx
+++ ignitefleet/App.tsx
@@ -14,8 +14,9 @@ import { StatusBar } from "react-native";
 
 import { AppProvider, UserProvider } from "@realm/react";
 import { REALM_APP_ID } from "@env";
-import { Home } from "./src/screens/Home";
+
 import { Routes } from "./src/routes";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -29,15 +30,17 @@ export default function App() {
   return (
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        {/* se nao estiver logado vai levar para o signin para o usuario logar, caso tenha leva a home */}
-        <UserProvider fallback={SignIn}>
-          <Routes />
-        </UserProvider>
+        <SafeAreaProvider>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          {/* se nao estiver logado vai levar para o signin para o usuario logar, caso tenha leva a home */}
+          <UserProvider fallback={SignIn}>
+            <Routes />
+          </UserProvider>
+        </SafeAreaProvider>
       </ThemeProvider>
     </AppProvider>
   );
--- ignitefleet/src/components/HomeHeader/index.tsx
+++ ignitefleet/src/components/HomeHeader/index.tsx
@@ -4,17 +4,22 @@ import { Power } from "phosphor-react-native";
 import theme from "../../theme";
 
 import { useUser, useApp } from "@realm/react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export function HomeHeader() {
   const user = useUser();
   const app = useApp();
+  /* pega a distancia da area segura, dos diferentes tipos de celulares */
+  const insets = useSafeAreaInsets();
+
+  const paddingTop = insets.top + 32;
 
   function handleLogout() {
     app.currentUser?.logOut();
   }
 
   return (
-    <Container>
+    <Container style={{ paddingTop }}>
       <Picture
         source={{ uri: user?.profile.pictureUrl }}
         placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
