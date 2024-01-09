e6ed4f3e6f4833c8b7e20e7124995f761eb54f67
gabriel <gabrielcomputacao@outlook.com>
2024-01-09 14:52:48 -0300

    feat:  adding ref input

--- ignitefleet/src/components/TextAreaInput/index.tsx
+++ ignitefleet/src/components/TextAreaInput/index.tsx
@@ -1,21 +1,32 @@
-import { TextInputProps } from "react-native";
+import { TextInput, TextInputProps } from "react-native";
 import { Container, Input, Label } from "./styles";
 import theme from "../../theme";
+import { forwardRef } from "react";
 
 type TextAreaInputProps = TextInputProps & {
   label: string;
 };
 
-export function TextAreaInput({ label }: TextAreaInputProps) {
-  return (
-    <Container>
-      <Label>{label}</Label>
+/* o forwardRef faz com que um componente criado personalizado receba um ref para ser passado para um componente react ou react-native
+  que possui a propriedade ref como padrao, ref pega a referencia da arvore da dom
+*/
 
-      <Input
-        multiline
-        autoCapitalize="sentences"
-        placeholderTextColor={theme.COLORS.GRAY_400}
-      />
-    </Container>
-  );
-}
+const TextAreaInput = forwardRef<TextInput, TextAreaInputProps>(
+  ({ label, ...rest }, ref) => {
+    return (
+      <Container>
+        <Label>{label}</Label>
+
+        <Input
+          ref={ref}
+          multiline
+          autoCapitalize="sentences"
+          placeholderTextColor={theme.COLORS.GRAY_400}
+          {...rest}
+        />
+      </Container>
+    );
+  }
+);
+
+export { TextAreaInput };
--- ignitefleet/src/screens/Departure/index.tsx
+++ ignitefleet/src/screens/Departure/index.tsx
@@ -1,24 +1,57 @@
+import { useRef } from "react";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
 import { TextAreaInput } from "../../components/TextAreaInput";
 import { Container, Content } from "./styles";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+} from "react-native";
 
 export function Departure() {
+  const descriptionRef = useRef<TextInput>(null);
+
+  const keyboardAvoidingViewBehavior =
+    Platform.OS === "android" ? "height" : "position";
+
+  function handleDepartureRegister() {}
+
   return (
     <Container>
       <Header title="Saída" />
 
-      <Content>
-        <LicensePlateInput label="Placa do Veículo" placeholder="BRSHD14" />
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={keyboardAvoidingViewBehavior}
+      >
+        <ScrollView>
+          <Content>
+            <LicensePlateInput
+              label="Placa do Veículo"
+              placeholder="BRSHD14"
+              /* icone do celular de clica em concluir esta nessa função , e pode ser modificada */
+              onSubmitEditing={() => descriptionRef.current?.focus()}
+              /* icone do botao do celular dentro do teclado */
+              returnKeyType="next"
+            />
 
-        <TextAreaInput
-          label="Finalidade"
-          placeholder="Vou utilizar o veículo para ..."
-        />
+            <TextAreaInput
+              ref={descriptionRef}
+              label="Finalidade"
+              placeholder="Vou utilizar o veículo para ..."
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType="send"
+              /* propriedade que faz com que quando o blur sai do componente quer dizer que ele vai fazer o submitting no caso de um multline */
+              blurOnSubmit
+            />
 
-        <Button title="Registrar Saída" />
-      </Content>
+            <Button title="Registrar Saída" onPress={handleDepartureRegister} />
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
