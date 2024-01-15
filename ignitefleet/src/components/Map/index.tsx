351b37561ed3cbae4a9d6a71356bda06e4da7b21
gabriel <gabrielcomputacao@outlook.com>
2024-01-15 06:50:00 -0300

    feat:  drawing line on map

--- ignitefleet/src/components/Map/index.tsx
+++ ignitefleet/src/components/Map/index.tsx
@@ -3,9 +3,12 @@ import MapView, {
   MapViewProps,
   Marker,
   PROVIDER_GOOGLE,
+  Polyline,
 } from "react-native-maps";
 import { IconBox } from "../IconBox";
-import { Car } from "phosphor-react-native";
+import { Car, FlagCheckered } from "phosphor-react-native";
+import { useRef } from "react";
+import theme from "../../theme";
 
 type Props = MapViewProps & {
   coordinates: LatLng[];
@@ -13,9 +16,20 @@ type Props = MapViewProps & {
 
 export function Map({ coordinates, ...rest }: Props) {
   const lastCoordinates = coordinates[coordinates.length - 1];
+  const mapRef = useRef<MapView>(null);
+
+  async function onMapLoaded() {
+    if (coordinates.length > 1) {
+      /* função para dois pontos marcados na tela se ajustarem dentro da tela */
+      mapRef.current?.fitToSuppliedMarkers(["departure", "arrival"], {
+        edgePadding: { top: 50, left: 50, right: 50, bottom: 50 },
+      });
+    }
+  }
 
   return (
     <MapView
+      ref={mapRef}
       /* utiliza o provider do google, para utilizar precisa pegar a chave da api de maps no google cloud plataform*/
       provider={PROVIDER_GOOGLE}
       style={{ width: "100%", height: 200 }}
@@ -25,11 +39,27 @@ export function Map({ coordinates, ...rest }: Props) {
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }}
+      onMapLoaded={onMapLoaded}
+      {...rest}
     >
       {/* adiciona o marcador dentro do mapa */}
-      <Marker coordinate={coordinates[0]}>
+      <Marker identifier="departure" coordinate={coordinates[0]}>
         <IconBox icon={Car} size="SMALL" />
       </Marker>
+
+      {coordinates.length > 1 && (
+        <>
+          <Marker identifier="arrival" coordinate={lastCoordinates}>
+            <IconBox icon={FlagCheckered} size="SMALL" />
+          </Marker>
+
+          <Polyline
+            coordinates={[...coordinates]}
+            strokeColor={theme.COLORS.GRAY_700}
+            strokeWidth={6}
+          />
+        </>
+      )}
     </MapView>
   );
 }
